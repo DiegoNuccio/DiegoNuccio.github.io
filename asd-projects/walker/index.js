8576845927ebecfd1e4fe17b37b0da2ef3f4db0f
DiegoNuccio <cadetdnuccio@nomma.net>
2023-06-12 16:58:06 +0000

    todo 2 + 3 complete

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,18 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEYS = { //This stores each of the number values of the arrows buttons
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  }
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +39,10 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEYS.LEFT) {
+      console.log("left arrow pressed");
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
